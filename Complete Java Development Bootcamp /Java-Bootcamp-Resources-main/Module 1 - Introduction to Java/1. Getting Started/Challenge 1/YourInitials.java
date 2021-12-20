59eda4ddc4740aea4ec152449901649303a4acee
vincent <vincent.arteaga@gmail.com>
2021-12-20 15:50:51 -0800

    2nd commit

---
+++ Complete Java Development Bootcamp /Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -0,0 +1,12 @@
+public class YourInitials {
+    public static void main(String[] args) {
+        //print initials here. 
+        System.out.println("V   V    A  ");
+        System.out.println("V   V   A A ");
+        System.out.println("V   V  A   A");
+        System.out.println("V   V  AAAAA");
+        System.out.println("V   V  A   A");
+        System.out.println(" V V   A   A");
+        System.out.println("  V    A   A");
+    }
+}
