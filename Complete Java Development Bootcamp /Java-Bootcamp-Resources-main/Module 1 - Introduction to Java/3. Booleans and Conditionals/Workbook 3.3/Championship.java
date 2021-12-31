354062a15a461d752c4960fe0b162eb4a19c5bc1
vincent <vincent.arteaga@gmail.com>
2021-12-30 17:32:19 -0800

    workbook 3.3 complete

--- Complete Java Development Bootcamp /Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.3/Championship.java
+++ Complete Java Development Bootcamp /Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.3/Championship.java
@@ -1,23 +1,34 @@
 public class Championship {
     public static void main(String[] args) {
 
-        int gryffindor = 400;    //gryffindor points
-        int ravenclaw = 200;    //ravenclaw points
+        int gryffindor = 450;    //gryffindor points
+        int ravenclaw = 500;    //ravenclaw points
 
         // int margin = amount of points by which gryffindor scored over ravenclaw;
-
+        int margin = gryffindor - ravenclaw;
 
         //if gryffindor wins by a margin of 300 points:
-             //print: Gryffindor takes the house cup!
-
-        //if gryffindor wins by a margin of any points: 
+        if (margin > 300) {
+            //print: Gryffindor takes the house cup!
+            System.out.println("Gryffindor takes the house cup!");
+             
+        } else if (gryffindor > ravenclaw) { //if gryffindor wins by a margin of any points:
             //print: In second place, Gryffindor!
+            System.out.println("In second place, Gryffindor!");
 
-        //if gryffindor loses by a margin of 100 points, they are third:
+        } else if (Math.abs(margin) <= 100){ //if gryffindor loses by a margin of 100 points, they are third:
             //print: In third place, Gryffindor!
+            System.out.println("In third place, Gryffindor!");
 
-        //else:
-           //print: In fourth place, Gryffindor! 
+        } else { //else:
+            //print: In fourth place, Gryffindor! 
+            System.out.println("In fourth place, Gryffindor!");
+        }
+        
+
+
+        
+
         
     }
 }
