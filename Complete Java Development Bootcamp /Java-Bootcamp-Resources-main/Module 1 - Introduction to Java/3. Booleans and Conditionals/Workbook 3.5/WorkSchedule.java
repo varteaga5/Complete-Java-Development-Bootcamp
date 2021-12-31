2e8948c244694f2b1686287156c2fda8b143f962
vincent <vincent.arteaga@gmail.com>
2021-12-30 19:28:57 -0800

    workbook 3.5 complete

--- Complete Java Development Bootcamp /Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.5/WorkSchedule.java
+++ Complete Java Development Bootcamp /Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.5/WorkSchedule.java
@@ -1,11 +1,18 @@
 public class WorkSchedule {
     public static void main(String[] args) {
-        int day = 3;  
-        boolean holiday = true;
+        int day = 6;  
+        boolean holiday = false;
          
         // if it's a holiday, print: "woohoo, no work!");
-        // if it's the weekend, print: "it's the weekend, no work!"
-        // otherwise, print: "Wake up at 7:00 :( ";
+        if (holiday) {
+            System.out.println("woohoo, i still work!");
+        } else if (day >= 6) {
+            // if it's the weekend, print: "it's the weekend, no work!"
+            System.out.println("it's the weekend, i still work!");
+        } else {
+            // otherwise, print: "Wake up at 7:00 :( ";
+            System.out.println("i still work!");
+        }
         }
 
     }
