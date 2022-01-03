19ab85258d39d1674223a06a97944f201617ec59
vincent <vincent.arteaga@gmail.com>
2022-01-02 23:19:55 -0800

    workbook part 3 complete

--- Complete Java Development Bootcamp /Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/7. The World of Objects/airline-reservation-system/Main.java
+++ Complete Java Development Bootcamp /Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/7. The World of Objects/airline-reservation-system/Main.java
@@ -4,14 +4,9 @@ public class Main {
   
     public static void main(String[] args) {
 
-        Person p1 = new Person("vaa", "usa", "42069", new String []{"vaa", "usa", "42069"}, 4 );
+        Person p1 = new Person("vaa", "usa", "42069", new String []{"vaa", "usa", "42069"}, 8 );
 
-        System.out.println(p1.name);
-        System.out.println(p1.dateOfBirth);
-        System.out.println(p1.nationality);
-        System.out.println(Arrays.toString(p1.passport));
-        System.out.println(p1.seatNumber);
-
-        System.out.println("Name: " + p1.name + "\n" + "Nationality: " + p1.nationality + "\n" + "Date of Birth: " + p1.dateOfBirth + "\n" + "Seat Number: " + p1.seatNumber + "\n");
+        p1.setSeatNumber(55);
+        System.out.println("Name: " + p1.getName() + "\n" + "Nationality: " + p1.getNationality() + "\n" + "Date of Birth: " + p1.getDateOfBirth() + "\n" + "Seat Number: " + p1.getSeatNumber() + "\n");
     }
 }
