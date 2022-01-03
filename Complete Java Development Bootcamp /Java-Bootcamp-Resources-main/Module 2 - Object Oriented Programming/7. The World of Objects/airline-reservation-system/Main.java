fca06514a810df5c6c1438f95e538d5716116a12
vincent <vincent.arteaga@gmail.com>
2022-01-02 19:11:51 -0800

    added constructor method

--- Complete Java Development Bootcamp /Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/7. The World of Objects/airline-reservation-system/Main.java
+++ Complete Java Development Bootcamp /Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/7. The World of Objects/airline-reservation-system/Main.java
@@ -4,21 +4,13 @@ public class Main {
   
     public static void main(String[] args) {
 
-        Person p1 = new Person();
-        p1.name = "tim";
-        p1.dateOfBirth = "420";
-        p1.nationality = "sri lanka";
-        p1.passport = new String []{"tim", "sri lanka", "420"};
-        p1.seatNumber = 4;
+        Person p1 = new Person("vaa", "usa", "42069", new String []{"vaa", "usa", "42069"}, 4 );
 
         System.out.println(p1.name);
         System.out.println(p1.dateOfBirth);
         System.out.println(p1.nationality);
         System.out.println(Arrays.toString(p1.passport));
         System.out.println(p1.seatNumber);
-      
-      
-      
     }
   
   
--- Complete Java Development Bootcamp /Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/7. The World of Objects/airline-reservation-system/Person.java
+++ Complete Java Development Bootcamp /Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/7. The World of Objects/airline-reservation-system/Person.java
@@ -5,6 +5,15 @@ public class Person {
     String[] passport;
     int seatNumber;
 
+    // Constructor method
+    public Person(String name, String nationality, String dateOfBirth, String[] passport, int seatNumber){
+        this.name = name;
+        this.nationality = nationality;
+        this.dateOfBirth = dateOfBirth;
+        this.passport = passport;
+        this.seatNumber = seatNumber;
+    }
+
     public void applyPassport(){
         System.out.println("passport");
     }
