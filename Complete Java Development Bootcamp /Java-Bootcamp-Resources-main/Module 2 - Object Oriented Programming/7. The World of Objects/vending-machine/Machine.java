f45523c492f2630edb1ac01b4f79264783da558d
vincent <vincent.arteaga@gmail.com>
2022-03-02 20:23:12 -0800

    vending machine complete

--- Complete Java Development Bootcamp /Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/7. The World of Objects/vending-machine/Machine.java
+++ Complete Java Development Bootcamp /Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/7. The World of Objects/vending-machine/Machine.java
@@ -1,7 +1,20 @@
 public class Machine {
-    
-    
-    
+    private Item[][] items;
+
+    public Machine(Item[][] items) {
+        this.items = new Item[items.length][items[0].length];
+        for (int i = 0; i < items.length; i++) {
+            for (int j = 0; j < items[i].length; j++) {
+                this.items[i][j] = new Item(items[i][j]);
+            }
+        }
+    }
+    public Item getItem(int row, int spot) {
+        return new Item(this.items[row][spot]);
+    }
+    public void setItem(Item item, int row, int spot) {
+        this.items[row][spot] = new Item(item);
+    }
     /**
      * Function name – dispense
      * @param row (int)
@@ -13,4 +26,24 @@ public class Machine {
      *      • if so: decreases its quantity by one and returns true.
      *      • otherwise: returns false.
      */
+    public boolean dispense(int row, int spot){
+        if (this.items[row][spot].getQuantity() > 0) {
+           this.items[row][spot].setQuantity(items[row][spot].getQuantity() - 1);
+           return true;
+        } 
+        return false;
+    }
+    public String toString() {
+        String temp = "";
+        for (int i = 0; i < items.length; i++) {
+            temp += "\t";
+            for (int j = 0; j < items[i].length; j++) {
+                temp += items[i][j].toString() + " ";
+            }
+            temp += "\n\n";
+        }
+        temp +=
+        "\t************************************************";
+        return temp;
+    }
 }
