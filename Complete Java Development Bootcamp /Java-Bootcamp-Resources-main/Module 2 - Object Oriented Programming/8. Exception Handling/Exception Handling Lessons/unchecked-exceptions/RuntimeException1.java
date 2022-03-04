cf0e2ebaf22e8727e0c74cd2bfc743696f43ceaa
vincent <vincent.arteaga@gmail.com>
2022-03-04 13:36:31 -0800

    runtime exception

--- Complete Java Development Bootcamp /Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Lessons/unchecked-exceptions/RuntimeException1.java
+++ Complete Java Development Bootcamp /Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Lessons/unchecked-exceptions/RuntimeException1.java
@@ -1,6 +1,16 @@
 public class RuntimeException1 {
     public static void main(String[] args) {
         int[] array = new int[3];
+      
+        // DO NOT DO THIS
+        // FIX THE CODE INSTEAD
+        try {
+            System.out.println(array[5]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            //TODO: handle exception
+            System.out.println(e.getMessage());
+
+        }
 
     }
 }
