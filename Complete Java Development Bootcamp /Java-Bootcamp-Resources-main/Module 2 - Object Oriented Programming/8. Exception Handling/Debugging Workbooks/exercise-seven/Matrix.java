8d137923643f008e5759596688f6f876df9441c3
vincent <vincent.arteaga@gmail.com>
2022-03-04 08:42:41 -0800

    debugging matrix

--- Complete Java Development Bootcamp /Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Debugging Workbooks/exercise-seven/Matrix.java
+++ Complete Java Development Bootcamp /Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Debugging Workbooks/exercise-seven/Matrix.java
@@ -8,9 +8,9 @@ public class Matrix {
              {0, 0, 0}
         };
 
-        for (int i = 0; i < matrix.length; i--) {
-            for (int j = 0; j < matrix.length; i++) {
-                matrix[i][j] = i;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                matrix[i][j] = j;
             }
         }
         printMatrix(matrix);
