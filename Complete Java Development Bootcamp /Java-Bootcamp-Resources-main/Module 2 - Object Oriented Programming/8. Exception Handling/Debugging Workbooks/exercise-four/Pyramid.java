f3823e7028554d39ce7675331ab710c74bc74a99
vincent <vincent.arteaga@gmail.com>
2022-03-03 22:16:42 -0800

    pyramid debuggin

--- Complete Java Development Bootcamp /Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Debugging Workbooks/exercise-four/Pyramid.java
+++ Complete Java Development Bootcamp /Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Debugging Workbooks/exercise-four/Pyramid.java
@@ -1,9 +1,15 @@
 public class Pyramid {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-          for (int j = 0; j < 10; j++) {
-              System.out.print(" * ");
+          for (int j = 0; j <= i; j++) {
+              if (i == j) {
+                  System.out.print(" * ");
+
+              } else {
+                  System.out.print(" ");
+              }
           }
+          System.out.print("\n");
         }
     }
 }
--- Complete Java Development Bootcamp /Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Debugging Workbooks/exercise-three/Functions.java
+++ Complete Java Development Bootcamp /Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Debugging Workbooks/exercise-three/Functions.java
@@ -2,17 +2,20 @@ public class Functions {
     public static void main(String[] args) {
         int random = random(15);    
         System.out.println("Number: " + random);
-        System.out.println("Prime or Composite: " + isPrime(random));
+        System.out.println("Prime or Composite: " + (random == 1 ? "COMPOSITE" : isPrime(random)));
     }
 
     public static int random(int range) {
-        double random = Math.random() + range + 1;
+        double random = Math.random() * range + 1;
         return (int) random;
     }
     public static String isPrime(int random) {
-        String isPrime = "";
-        for (int i = 0; i < random; i++) {
-            isPrime = (random % i == 0) ? "COMPOSITE" : "PRIME";
+        String isPrime = "PRIME";
+        for (int i = 2; i < random; i++) {
+           if (random % i == 0) {
+               isPrime = "COMPOSITE";
+               break;
+           }
         }
         return isPrime;
     }
