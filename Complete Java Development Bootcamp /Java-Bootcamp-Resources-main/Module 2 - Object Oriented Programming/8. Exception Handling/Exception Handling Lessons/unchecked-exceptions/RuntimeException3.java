2ec2e7b4c5fa630f1b857f849003cfa1d527610c
vincent <vincent.arteaga@gmail.com>
2022-03-04 14:48:09 -0800

    runtime exception rule of thumb

--- Complete Java Development Bootcamp /Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Lessons/unchecked-exceptions/RuntimeException3.java
+++ Complete Java Development Bootcamp /Java-Bootcamp-Resources-main/Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Lessons/unchecked-exceptions/RuntimeException3.java
@@ -3,7 +3,17 @@ public class RuntimeException3 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Please enter a number: ");
+        if (scan.hasNextInt()) {
+            System.out.println(scan.nextInt());
+        } else {
+            scan.nextLine();
+            System.out.println("not a number");
+        }
         System.out.println(scan.nextInt());
         scan.close();
+
+        // RULE OF THUMB
+        // catch checked exceptions (these are outside of the applications control)
+        // fix unchecked exceptions (this is badly written code)
     }
 }
